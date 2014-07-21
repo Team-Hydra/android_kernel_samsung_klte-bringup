6c0b1f87c214e11cceac7a5c7929502f42fe69b0
Matt Filetto <matt.filetto@gmail.com>
2014-07-21 09:21:34 -0700

    Linux 3.4.0-> 3.4.99
    Conflicts:
    	kernel/futex.c
    	net/netlink/af_netlink.c
    
    Change-Id: Ib48dcd111d94f75593c813f70f2056922685ea4e

--- arch/ia64/kernel/process.c
+++ arch/ia64/kernel/process.c
@@ -29,6 +29,10 @@
 #include <linux/kdebug.h>
 #include <linux/utsname.h>
 #include <linux/tracehook.h>
+<<<<<<< HEAD
+=======
+#include <linux/rcupdate.h>
+>>>>>>> eeac748... Linux 3.4.0-> 3.4.99
 
 #include <asm/cpu.h>
 #include <asm/delay.h>
@@ -301,6 +305,10 @@ cpu_idle (void)
 
 	/* endless idle loop with no priority at all */
 	while (1) {
+<<<<<<< HEAD
+=======
+		rcu_idle_enter();
+>>>>>>> eeac748... Linux 3.4.0-> 3.4.99
 		if (can_do_pal_halt) {
 			current_thread_info()->status &= ~TS_POLLING;
 			/*
@@ -331,6 +339,10 @@ cpu_idle (void)
 			normal_xtp();
 #endif
 		}
+<<<<<<< HEAD
+=======
+		rcu_idle_exit();
+>>>>>>> eeac748... Linux 3.4.0-> 3.4.99
 		schedule_preempt_disabled();
 		check_pgt_cache();
 		if (cpu_is_offline(cpu))
--- arch/m68k/kernel/process.c
+++ arch/m68k/kernel/process.c
@@ -25,6 +25,10 @@
 #include <linux/reboot.h>
 #include <linux/init_task.h>
 #include <linux/mqueue.h>
+<<<<<<< HEAD
+=======
+#include <linux/rcupdate.h>
+>>>>>>> eeac748... Linux 3.4.0-> 3.4.99
 
 #include <asm/uaccess.h>
 #include <asm/traps.h>
@@ -75,8 +79,15 @@ void cpu_idle(void)
 {
 	/* endless idle loop with no priority at all */
 	while (1) {
+<<<<<<< HEAD
 		while (!need_resched())
 			idle();
+=======
+		rcu_idle_enter();
+		while (!need_resched())
+			idle();
+		rcu_idle_exit();
+>>>>>>> eeac748... Linux 3.4.0-> 3.4.99
 		schedule_preempt_disabled();
 	}
 }
